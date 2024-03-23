4a766cbf0f2e35821a2f2786702ef34786dd4adb
wissseman777 <161206102+wissseman777@users.noreply.github.com>
2024-03-23 10:19:39 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+    }
+        int[] data = new int[10];
+
+}
+
+public class RandomDataGenerator {
+        }
